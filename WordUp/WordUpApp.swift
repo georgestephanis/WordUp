0c1d64b82c6c040a52a6f7c879a503dd0abed098
George Stephanis <daljo628@gmail.com>
2025-12-05 14:01:47 -0500

    Initial Commit

---
+++ WordUp/WordUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordUpApp.swift
+//  WordUp
+//
+//  Created by George Stephanis on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct WordUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
